7a4c3bc27fb09242883a5f8704aaa7a1e93f88c9
unntangle <unntangle20@gmail.com>
2026-07-14 12:55:11 +0530

    feat: change header to fixed positioning so hero image flows behind it without white space

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -52,7 +52,7 @@ export function Navbar() {
 
   return (
     <header
-      className="sticky top-0 z-50 w-full py-4 px-4 md:px-6 bg-transparent"
+      className="fixed top-0 left-0 right-0 z-50 py-4 px-4 md:px-6 bg-transparent"
       onMouseLeave={() => {
         setMegaOpen(false);
         setMoreOpen(false);
